29a98f1db84de0024d8d0a9018fa6e9c9947870d
Piotr Zając <piotr.zajac@dotpay.pl>
2019-07-26 09:30:42 +0200

    enabling apache mods, adding php7.3-apache image

--- dist/7.1-apache/bin/init.sh
+++ dist/7.1-apache/bin/init.sh
@@ -10,3 +10,11 @@ if [[ -n ${http_proxy} ]]; then
 fi
 
 curl ${CURL_ARGS} --location --output /sbin/tini https://github.com/krallin/tini/releases/download/${TINI_VERSION}/tini && chmod +x /sbin/tini
+
+a2enmod rewrite && \
+# -- Enable mod_status
+a2enmod status && \
+# -- Enable mod_header
+a2enmod headers && \
+# -- Enable request ID
+a2enmod unique_id
